067dd3b61e7a7011c61b0e69e5fb2fbaa4107237
souls-syntax <souls.syntax@gmail.com>
2025-09-29 21:23:15 +0530

    Folder structure

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <array>
+#include <string>
+#include <memory>
+#include <cstdio>
+#include <sstream>
+#include <stdexcept>
+
+using namespace std;
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << a << " " << b << " " << c <<"\n"; 
+}
